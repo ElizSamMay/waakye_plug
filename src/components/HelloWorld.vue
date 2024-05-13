379b107b488ccf6ccf0a5a5a44dc3a5b7a1e2f9c
ElizSamMay <kobbywesley1@gmail.com>
2024-05-13 15:21:27 +0000

    added homepage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+ <section class="firstSec">
+        <div>
+            <img class="headerpic" src="../assets/Waakyeplate-homepge.png" >
+        </div>
+    </section>
+
+    <section class="secondSec">
+        <div class="text">
+            <p class="text1">Your no. 1 Waakye Plug.</p>
+        </div>
+        <div class="text">
+            <P class="text2">Feel the taste of some of the nicest waakye
+             in Accra at the comfort of your home.</P> 
+        </div>
+        <div class="text">
+            <p class="text3" >Thinking of payment 🤔 ?, don’t stress, 
+            <span  class="edit"> We take payment upon delivery.</span></P> 
+        </div>
+
+    </section>
+
+
+    <section class="lastSec">
+        <div class="butt"> 
+            <button class="editbutt">Get Waakye</button>
+        </div>
+
+    </section>
+    
 </template>
 
 <script>
@@ -41,18 +41,62 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ .firstSec{
+            border: 1px solid black;
+            background-color: rgb(31, 31, 70);
+        }
+        .headerpic{
+            width: 200px;
+        }
+        .secondSec{
+            border: 1px solid black; 
+            background-color: rgb(31, 31, 70);
+            color: white;
+            
+        }
+        .text{
+           
+            border: 1px solid black;
+            padding: 20px;
+            
+        }
+        .text1{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 35px;
+            font-weight: 700;
+            
+
+        }
+        .text2,.text3{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 15px;
+            font-weight: 500; 
+        }
+        .edit{
+            font-size: 20px;
+            font-weight: 700;  
+        }
+        .lastSec{
+            border: 1px solid black;
+            padding: 120px; 
+            position: relative;
+            background-color: rgb(31, 31, 70);
+        }
+        .butt{
+            border: 1px solid black;
+            padding: 10px;  
+            position: absolute;
+            bottom: 15px;  
+            left:  70px; 
+        }
+        .editbutt{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 20px;
+            font-weight: 550;
+            border: 1px solid black;
+            border-radius: 100px;
+            width: 200px; 
+            height: 35px;
+        }
+
 </style>
