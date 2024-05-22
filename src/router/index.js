d0fbb23aec33b591876323dad4c991b54ecb7f21
ElizSamMay <kobbywesley1@gmail.com>
2024-05-22 01:56:39 +0000

    added subtotal

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const router = createRouter({
         },
         {
             path: '/sub-total',
-            name: 'subTotal',
+            name: 'subtotal',
             component: subtotal
         },
         {
