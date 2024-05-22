67582b19e18cfc7d6aa8138a5fee809ac4696884
ElizSamMay <kobbywesley1@gmail.com>
2024-05-22 15:15:42 +0000

    added Thankyou

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import {createMemoryHistory, createRouter} from 'vue-router'
 import HomePage from '../components/HomePage.vue';
 import AddDishes from '../components/AddDishes.vue';
 import SubtotalSection from '../components/SubtotalSection.vue';
-import ThankYou from '../components/ThankYou.vue';
+import ThankYou from './../components/ThankYou.vue';
 
 
 
