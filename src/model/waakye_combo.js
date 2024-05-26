94a334c5cffdda6448a79673e6592c881e4f09fa
ElizSamMay <kobbywesley1@gmail.com>
2024-05-26 13:43:23 +0000

    added waakyecombo

--- src/model/waakye_combo.js
+++ src/model/waakye_combo.js
@@ -3,7 +3,7 @@ export const waakyeCombos = [
     {   
         id: 0,
         title: "Waakye Combo", 
-    ingredients: ["waakye", "wale", "egg","sausage","salad","gari" ],
+    ingredients: ["waakye", "wole", "egg","sausage","salad","gari" ],
         price: 30, 
         isSelected: true
     },
@@ -11,7 +11,7 @@ export const waakyeCombos = [
     {   
         id: 1,
         title: "Waakye Super", 
-        ingredients: ["waakye", "cassava fish","wale", "egg",  "salad","macaroni", "gari"],
+        ingredients: ["waakye", "cassava fish","wole", "egg",  "salad","macaroni", "gari"],
         price: 41, 
         isSelected: false
     },
@@ -19,7 +19,7 @@ export const waakyeCombos = [
     {   
         id: 4,
         title: "Waakye Capo", 
-        ingredients: ["waakye", "chicken","wale", "egg",  "salad","macaroni", "gari"],
+        ingredients: ["waakye", "chicken","wole", "egg",  "salad","macaroni", "gari"],
         price: 41, 
         isSelected: false
     },
@@ -27,7 +27,7 @@ export const waakyeCombos = [
     {   
         id: 5,
         title: "Waakye Gooda", 
-        ingredients: ["waakye", "meat","wale", "egg",  "salad","macaroni", "gari"],
+        ingredients: ["waakye", "meat","wole", "egg",  "salad","macaroni", "gari"],
         price: 41, 
         isSelected: false
     },
@@ -35,14 +35,14 @@ export const waakyeCombos = [
     {   
         id: 2,
         title: "Waakye Mega", 
-        ingredients: ["waakye", "cassava fish","meat", "egg", "wale", "salad","macaroni", "gari"],
+        ingredients: ["waakye", "cassava fish","meat", "egg", "wole", "salad","macaroni", "gari"],
         price: 55, 
         isSelected: false
     },
     {   
         id: 3,
         title: "Waakye Agadoo", 
-        ingredients: ["waakye", "cassava fish", "meat", "egg","chicken", "wale", "salad","macaroni", "gari","wagashi"],
+        ingredients: ["waakye", "cassava fish", "meat", "egg","chicken", "wole", "salad","macaroni", "gari","wagashi"],
         price: 75, 
         isSelected: false
     },
@@ -51,13 +51,13 @@ export const waakyeCombos = [
 
 
 export const ingredients = [
-    {id:1,title: "Salad", isSelected: false, image: "", price: 10},
-    {id:2,title: "Macroni", isSelected: false, image: "", price: 10},
-    {id:3,title: "Sausage", isSelected: false, image: "", price: 10},
-    {id:4,title: "Fish", isSelected: false, image: "", price: 10},
-    {id:6,title: "Meat", isSelected: false, image: "", price: 10},
-    {id:7,title: "Egg", isSelected: false, image: "https://www.simplyrecipes.com/thmb/zsQvDavpqD2PtIO-7W6nBWVHCe4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Hard-Boiled-Eggs-LEAD-03-42506773297f4a15920c46628d534d67.jpg", price: 10},
-    {id:8,title: "Wagashi", isSelected: false, image: "", price: 10},
-    {id:9,title: "Wole", isSelected: false, image: "", price: 10},
-    {id:10,title: "Gari", isSelected: false, image: "", price: 10},
+    {id:1,title: "Salad", isSelected: false, image: "", price: 4},
+    {id:2,title: "Macroni", isSelected: false, image: "", price: 4},
+    {id:3,title: "Sausage", isSelected: false, image: "", price: 4},
+    {id:4,title: "Fish", isSelected: false, image: "", price: 12},
+    {id:6,title: "Meat", isSelected: false, image: "", price: 15},
+    {id:7,title: "Egg", isSelected: false, image: "https://www.simplyrecipes.com/thmb/zsQvDavpqD2PtIO-7W6nBWVHCe4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Hard-Boiled-Eggs-LEAD-03-42506773297f4a15920c46628d534d67.jpg", price: 5},
+    {id:8,title: "Wagashi", isSelected: false, image: "", price: 6},
+    {id:9,title: "Wole", isSelected: false, image: "", price: 6},
+    {id:10,title: "Gari", isSelected: false, image: "", price: 4},
 ]
