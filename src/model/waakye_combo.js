371284bf65615760ffa8839214d6967612bcc36c
ElizSamMay <kobbywesley1@gmail.com>
2024-05-26 16:53:49 +0000

    added updated price

--- src/model/waakye_combo.js
+++ src/model/waakye_combo.js
@@ -51,13 +51,13 @@ export const waakyeCombos = [
 
 
 export const ingredients = [
-    {id:1,title: "Salad", isSelected: false, image: "", price: 10},
-    {id:2,title: "Macroni", isSelected: false, image: "", price: 10},
-    {id:3,title: "Sausage", isSelected: false, image: "", price: 10},
-    {id:4,title: "Fish", isSelected: false, image: "", price: 10},
-    {id:6,title: "Meat", isSelected: false, image: "", price: 10},
-    {id:7,title: "Egg", isSelected: false, image: "https://www.simplyrecipes.com/thmb/zsQvDavpqD2PtIO-7W6nBWVHCe4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Hard-Boiled-Eggs-LEAD-03-42506773297f4a15920c46628d534d67.jpg", price: 10},
-    {id:8,title: "Wagashi", isSelected: false, image: "", price: 10},
-    {id:9,title: "Wole", isSelected: false, image: "", price: 10},
-    {id:10,title: "Gari", isSelected: false, image: "", price: 10},
+    {id:1,title: "Salad", isSelected: false, image: "https://www.simplyrecipes.com/thmb/j0ddRCJrN78xVUXQ1qKqfohVo0A=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Caesar-Salad-LEAD-07-01d96a42f43941748b81d910a10888cd.jpg", price: 3},
+    {id:2,title: "Macroni", isSelected: false, image: "https://www.saveur.com/uploads/2022/06/08/00-FEATURE-BEST-PASTA-BOWLS-scaled.jpg?auto=webp&width=2000&height=1334", price: 3},
+    {id:3,title: "Sausage", isSelected: false, image: "https://www.meatsandsausages.com/public/images/sausage-recipes/sausage-links.jpg", price: 4},
+    {id:4,title: "Fish", isSelected: false, image: "https://i.ytimg.com/vi/LYfws791D80/maxresdefault.jpg", price: 12},
+    {id:6,title: "Meat", isSelected: false, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxqPMtLNi5Q_PxUZJz5ieRiuYD-EnK2RF1kYCu58IGUA&s", price: 15},
+    {id:7,title: "Egg", isSelected: false, image: "https://www.simplyrecipes.com/thmb/zsQvDavpqD2PtIO-7W6nBWVHCe4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Hard-Boiled-Eggs-LEAD-03-42506773297f4a15920c46628d534d67.jpg", price: 4},
+    {id:8,title: "Wagashi", isSelected: false, image: "https://pbs.twimg.com/media/EhtjmWDXYAEIb_L.jpg", price: 7},
+    {id:9,title: "Wole", isSelected: false, image: "https://i.ytimg.com/vi/dL9r9mKPcTQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCcJG3IPdnXk-hL-y7xGsgWWqW7RA", price: 7},
+    {id:10,title: "Gari", isSelected: false, image: "https://www.homefoodsghana.com/images/food/gari-foto.jpg", price: 3},
 ]
